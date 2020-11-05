3669338f53d3fc40a3335b84975c940f608ec6f5
Adel Elmasery <a_elmasery@hotmail.com>
2020-11-05 07:23:53 -0500

    Login page added

--- banking-app/src/App.js
+++ banking-app/src/App.js
@@ -1,17 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { React, Component } from "react";
+import Login from './login/login';
 import {Card,Nav } from './homepage/hompage';
 
 
-function App() {
-  return (
-    <div className="container">
-      {/* <header className="App-header"> */}
-        <Card/>
-        <Nav/>
-      {/* </header> */}
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+        auth: false
+    }
+    this.handleLogin = this.handleLogin.bind(this);
+  }
 
-export default App;
+  handleLogin = (loginEvent) => {
+    this.setState({ auth: loginEvent});
+  }
+
+  render(){
+    return (
+      <div className="container">
+          {this.state.auth
+          ?
+            <div>
+              <Card/>
+              <Nav/>
+            </div>
+          : <Login onLoginEvent={this.handleLogin}/>
+          }
+      </div>
+    );
+  }
+}
