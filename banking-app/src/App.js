cbd5e3d8d490f9dfb9b66f515a1b090a096a0be3
Lenroy <lenroy@Lenroys-MacBook-Air.local>
2020-11-05 07:40:10 -0500

    update button

--- banking-app/src/App.js
+++ banking-app/src/App.js
@@ -5,17 +5,30 @@ import {Card,Nav } from './homepage/hompage';
 import './sidebar/sidebar.css'
 import TransactionsList from './transactions/TransactionsList';
 
+const CARD ='CARD'
+const TRANSACTION='TRANSACTION'
+var view=CARD;
+
+function setVeiw(navView){
+    view=navView;
+    
+  }
+
 function App() {
+
+
+  
+
   return (
     <div className="App" id="outer-container">
       <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
       <div className="container">
         {/* <header className="App-header"> */}
         <div id="page-wrap">
-          <h1>Yes Bank</h1>        
-            <Card/>
-            <TransactionsList/>
-            <Nav/>
+  <h1>Yes Bank {view}</h1>        
+            {view===CARD&&<Card/>}
+           {view===TRANSACTION&& <TransactionsList/>}
+            <Nav appCallBack={setVeiw}/>
         {/* </header> */}
         </div>
       </div>
